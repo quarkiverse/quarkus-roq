33e67ee600ad11639434395149bedb0f1c4a0a21
Andy Damevin <ia3andy@gmail.com>
2024-10-10 09:30:52 +0200

    Redirect with meta

--- blog/src/test/java/io/quarkiverse/roq/it/RoqTest.java
+++ blog/src/test/java/io/quarkiverse/roq/it/RoqTest.java
@@ -12,7 +12,7 @@ public class RoqTest {
     @Test
     public void testIndex() {
         given().when().get("/").then().statusCode(200).body(containsString(
-                "I provide you with all the tools to generate static websites out of your Quarkus web application."))
+                        "I provide you with all the tools to generate static websites out of your Quarkus web application."))
                 .body(containsString("Hello, world! I&#39;m Roq")).body(containsString("minute read"))
                 .body(containsString("Page 1 of")).body(containsString("2024 &copy; ROQ"));
     }
@@ -25,7 +25,7 @@ public class RoqTest {
     @Test
     public void testPosts() {
         given().when().get("/posts/2024-08-29-welcome-to-roq").then().statusCode(200).body(containsString(
-                "I provide you with all the tools to generate static websites out of your Quarkus web application."))
+                        "I provide you with all the tools to generate static websites out of your Quarkus web application."))
                 .body(containsString("<p>Hello folks,</p>"))
                 .body(containsString("<h1 class=\"page-title\">Welcome to Roq!</h1>"))
                 .body(containsString("2024 &copy; ROQ"));
@@ -34,17 +34,14 @@ public class RoqTest {
     @Test
     public void testPage() {
         given().when().get("/events").then().statusCode(200).body(containsString(
-                "I provide you with all the tools to generate static websites out of your Quarkus web application."))
+                        "I provide you with all the tools to generate static websites out of your Quarkus web application."))
                 .body(containsString("<h2 class=\"event-title\">Roq 1.0 Beta</h2>"))
                 .body(containsString("2024 &copy; ROQ"));
     }
 
     @Test
     public void testAlias() {
-        given().when().get("/first-roq-article-ever").then().statusCode(200).body(containsString(
-                "I provide you with all the tools to generate static websites out of your Quarkus web application."))
-                .body(containsString("<p>Hello folks,</p>"))
-                .body(containsString("<h1 class=\"page-title\">Welcome to Roq!</h1>"))
-                .body(containsString("2024 &copy; ROQ"));
+        given().when().get("/first-roq-article-ever").then().statusCode(200)
+                .body(containsString("url=\"/posts/2024-08-29-welcome-to-roq\""));
     }
 }
