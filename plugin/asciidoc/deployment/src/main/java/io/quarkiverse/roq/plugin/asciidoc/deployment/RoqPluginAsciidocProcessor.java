0992c734b16097fced43d7c049f799b27cf6d1c8
melloware <mellowaredev@gmail.com>
2024-10-23 09:48:01 -0400

    AsciiDoc native testing

--- plugin/asciidoc/deployment/src/main/java/io/quarkiverse/roq/plugin/asciidoc/deployment/RoqPluginAsciidocProcessor.java
+++ plugin/asciidoc/deployment/src/main/java/io/quarkiverse/roq/plugin/asciidoc/deployment/RoqPluginAsciidocProcessor.java
@@ -1,7 +1,14 @@
 package io.quarkiverse.roq.plugin.asciidoc.deployment;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.IndexDependencyBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 
 public class RoqPluginAsciidocProcessor {
 
@@ -10,6 +17,22 @@ public class RoqPluginAsciidocProcessor {
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    void indexTransitiveDependencies(BuildProducer<IndexDependencyBuildItem> index) {
+        index.produce(new IndexDependencyBuildItem("io.yupiik.maven", "asciidoc-java"));
+        index.produce(new IndexDependencyBuildItem("io.yupiik.maven", "ascii2svg"));
+    }
+
+    @BuildStep
+    void registerOpenPdfForReflection(BuildProducer<ReflectiveClassBuildItem> reflectiveClass,
+            CombinedIndexBuildItem combinedIndex) {
+        final List<String> classNames = new ArrayList<>();
+        classNames.add(io.yupiik.tools.ascii2svg.Svg.class.getName());
+
+        reflectiveClass.produce(
+                ReflectiveClassBuildItem.builder(classNames.toArray(new String[0])).methods().fields().build());
     }
 
 }
