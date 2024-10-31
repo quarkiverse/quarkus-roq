f395079c49600f59f6d56e5277fa4fcf673adccb
Andy Damevin <ia3andy@gmail.com>
2024-10-31 16:08:13 +0100

    Nice events and Quarkus Insight (#235)

--- blog/src/main/java/Events.java
+++ blog/src/main/java/Events.java
@@ -5,7 +5,7 @@ import java.util.List;
 @DataMapping(value = "events", parentArray = true)
 public record Events(List<Event> list) {
 
-    public record Event(String title, String description, String date) {
+    public record Event(String title, String description, String date, String link) {
 
         public LocalDate parsedDate() {
             return LocalDate.parse(date);
