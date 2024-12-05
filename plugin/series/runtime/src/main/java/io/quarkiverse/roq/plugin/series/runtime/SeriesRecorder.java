41f40c6aada3fb5ae65b4c9275858a02f1c1c9ce
Andy Damevin <ia3andy@gmail.com>
2024-12-05 17:38:21 +0100

    Rename to Series and Styling

---
+++ plugin/series/runtime/src/main/java/io/quarkiverse/roq/plugin/series/runtime/SeriesRecorder.java
@@ -0,0 +1,19 @@
+package io.quarkiverse.roq.plugin.series.runtime;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import io.quarkus.runtime.RuntimeValue;
+import io.quarkus.runtime.annotations.Recorder;
+
+@Recorder
+public class SeriesRecorder {
+    public RuntimeValue<Series> generateSeries(Map<String, List<String>> series) {
+        return new RuntimeValue<>(
+                new Series(series.entrySet().stream()
+                        .collect(Collectors.toMap(Map.Entry::getKey,
+                                entry -> new Series.SeriesEntry(entry.getKey(), entry.getValue())))));
+    }
+
+}
