cf5f0ce92780cadbf439e04bb2c4ac6d969662d2
jtama <j.tama@groupeonepoint.com>
2025-03-02 16:15:10 +0100

    so many code copy/pasted from antora lunr...

--- blog/src/main/resources/web/app/main.js
+++ blog/src/main/resources/web/app/main.js
@@ -7,7 +7,8 @@ hljs.highlightAll();
 var idx = null;
 var documents = null;
 const searchInput = document.getElementById('search')
-const searchResultContainer = document.createElement('dialog')
+const searchResultContainer = document.createElement('div')
+const snippetLength = 100
 searchResultContainer.classList.add('search-result-dropdown-menu')
 searchInput.parentNode.appendChild(searchResultContainer)
 const facetFilterInput = document.querySelector('#search-field input[type=checkbox][data-facet-filter]')
@@ -41,23 +42,57 @@ function createSearchResult (result, store, searchResultDataset) {
     let currentComponent
     result.forEach(function (item) {
         const doc = store.documents[item.ref]
-        let sectionTitle
-        searchResultDataset.appendChild(createSearchResultItem(doc, sectionTitle, item))
+        const metadata = item.matchData.metadata
+        searchResultDataset.appendChild(createSearchResultItem(doc, item, highlightHit(metadata, doc.title, doc)))
     })
 }
 
-function createSearchResultItem (doc, sectionTitle, item) {
+function highlightHit (searchMetadata, sectionTitle, doc) {
+    const terms = {}
+    for (const term in searchMetadata) {
+        const fields = searchMetadata[term]
+        for (const field in fields) {
+            terms[field] = [...(terms[field] || []), term]
+        }
+    }
+    return {
+        pageTitleNodes: highlightPageTitle(doc.title, terms.title || []),
+        pageContentNodes: highlightText(doc, terms.text || []),
+        //pageKeywordNodes: highlightTags(doc, terms.keyword || []),
+    }
+}
+
+
+function createSearchResultItem (doc, item, highlightingResult) {
+    const documentTitle = document.createElement('div')
+    documentTitle.classList.add('search-result-document-title')
+    highlightingResult.pageTitleNodes.forEach(function (node) {
+        let element
+        if (node.type === 'text') {
+            element = document.createTextNode(node.text)
+        } else {
+            element = document.createElement('span')
+            element.classList.add('search-result-highlight')
+            element.innerText = node.text
+        }
+        documentTitle.appendChild(element)
+    })
+    const documentHit = document.createElement('div')
+    documentHit.classList.add('search-result-document-hit')
     const documentHitLink = document.createElement('a')
     documentHitLink.href = doc.url
-    documentHitLink.text = doc.title
+    documentHit.appendChild(documentHitLink)
+    highlightingResult.pageContentNodes.forEach((node) => createHighlightedText(node, documentHitLink))
     const searchResultItem = document.createElement('div')
     searchResultItem.classList.add('search-result-item')
-    searchResultItem.appendChild(documentHitLink)
+    searchResultItem.appendChild(documentTitle)
+    searchResultItem.appendChild(documentHit)
     searchResultItem.addEventListener('mousedown', function (e) {
         e.preventDefault()
     })
     return searchResultItem
 }
+
 
 function createNoResult (text) {
     const searchResultItem = document.createElement('div')
@@ -158,19 +193,16 @@ function confineEvent (e) {
     e.stopPropagation()
 }
 
-function debounce (func, wait, immediate) {
+function debounce (func, wait) {
     let timeout
     return function () {
         const context = this
         const args = arguments
         const later = function () {
             timeout = null
-            if (!immediate) func.apply(context, args)
+            func.apply(context, args)
         }
-        const callNow = immediate && !timeout
-        clearTimeout(timeout)
         timeout = setTimeout(later, wait)
-        if (callNow) func.apply(context, args)
     }
 }
 
@@ -191,7 +223,7 @@ function executeSearch (index) {
     try {
         if (!query) return clearSearchResults()
         searchIndex(index.index, index.store, query)
-        searchResultContainer.showModal()
+        searchResultContainer.style.display = 'block';
     } catch (err) {
         if (err instanceof lunr.QueryParseError) {
         } else {
@@ -222,8 +254,11 @@ function initSearch (lunr, data) {
         'keydown',
         debounce(function (e) {
             if (e.key === 'Escape' || e.key === 'Esc') return clearSearchResults(true)
-            executeSearch(index)
-        }, 100)
+            if (e.which <= 90 && e.which >= 48) {
+                executeSearch(index)
+            }
+
+        }, 300)
     )
     searchInput.addEventListener('click', confineEvent)
     searchResultContainer.addEventListener('click', confineEvent)
@@ -233,6 +268,162 @@ function initSearch (lunr, data) {
     }
     document.documentElement.addEventListener('click', clearSearchResults)
 }
+
+
+function highlightPageTitle (title, terms) {
+    const positions = getTermPosition(title, terms)
+    return buildHighlightedText(title, positions, snippetLength)
+}
+
+function highlightText (doc, terms) {
+    const text = doc.content
+    const positions = getTermPosition(text, terms)
+    return buildHighlightedText(text, positions, snippetLength)
+}
+
+function getTermPosition (text, terms) {
+    const positions = terms
+        .map((term) => findTermPosition(lunr, term, text))
+        .filter((position) => position.length > 0)
+        .sort((p1, p2) => p1.start - p2.start)
+
+    if (positions.length === 0) {
+        return []
+    }
+    return positions
+}
+
+/**
+ * Taken and adapted from: https://github.com/olivernn/lunr.js/blob/aa5a878f62a6bba1e8e5b95714899e17e8150b38/lib/tokenizer.js#L24-L67
+ * @param lunr
+ * @param text
+ * @param term
+ * @return {{start: number, length: number}}
+ */
+function findTermPosition (lunr, term, text) {
+    const str = text.toLowerCase()
+    const index = str.indexOf(term)
+
+    if (index >= 0) {
+        // extend term until word boundary to return the entire word
+        const boundaries = str.substr(index).match(/^[\p{Alpha}]+/u)
+        if (boundaries !== null && boundaries.length >= 0) {
+            return {
+                start: index,
+                length: boundaries[0].length,
+            }
+        }
+    }
+
+    // Not found
+    return {
+        start: 0,
+        length: 0,
+    }
+}
+
+/**
+ * Splitting the text by the given positions.
+ * The text within the positions getting the type "mark", all other text gets the type "text".
+ * @param {string} text
+ * @param {Object[]} positions
+ * @param {number} positions.start
+ * @param {number} positions.length
+ * @param {number} snippetLength Maximum text length for text in the result.
+ * @returns {[{text: string, type: string}]}
+ */
+function buildHighlightedText (text, positions, snippetLength) {
+    const textLength = text.length
+    const validPositions = positions.filter(
+        (position) => position.length > 0 && position.start + position.length <= textLength
+    )
+
+    if (validPositions.length === 0) {
+        return [
+            {
+                type: 'text',
+                text:
+                    text.slice(0, snippetLength >= textLength ? textLength : snippetLength) +
+                    (snippetLength < textLength ? '...' : ''),
+            },
+        ]
+    }
+
+    const orderedPositions = validPositions.sort((p1, p2) => p1.start - p2.start)
+    const range = {
+        start: 0,
+        end: textLength,
+    }
+    const firstPosition = orderedPositions[0]
+    if (snippetLength && text.length > snippetLength) {
+        const firstPositionStart = firstPosition.start
+        const firstPositionLength = firstPosition.length
+        const firstPositionEnd = firstPositionStart + firstPositionLength
+
+        range.start = firstPositionStart - snippetLength < 0 ? 0 : firstPositionStart - snippetLength
+        range.end = firstPositionEnd + snippetLength > textLength ? textLength : firstPositionEnd + snippetLength
+    }
+    const nodes = []
+    if (firstPosition.start > 0) {
+        nodes.push({
+            type: 'text',
+            text: (range.start > 0 ? '...' : '') + text.slice(range.start, firstPosition.start),
+        })
+    }
+    let lastEndPosition = 0
+    const positionsWithinRange = orderedPositions.filter(
+        (position) => position.start >= range.start && position.start + position.length <= range.end
+    )
+
+    for (const position of positionsWithinRange) {
+        const start = position.start
+        const length = position.length
+        const end = start + length
+        if (lastEndPosition > 0) {
+            // create text Node from the last end position to the start of the current position
+            nodes.push({
+                type: 'text',
+                text: text.slice(lastEndPosition, start),
+            })
+        }
+        nodes.push({
+            type: 'mark',
+            text: text.slice(start, end),
+        })
+        lastEndPosition = end
+    }
+    if (lastEndPosition < range.end) {
+        nodes.push({
+            type: 'text',
+            text: text.slice(lastEndPosition, range.end) + (range.end < textLength ? '...' : ''),
+        })
+    }
+
+    return nodes
+}
+
+/**
+ * Creates an element from a highlightingResultNode and add it to the targetNode.
+ * @param {Object} highlightingResultNode
+ * @param {String} highlightingResultNode.type - type of the node
+ * @param {String} highlightingResultNode.text
+ * @param {Node} targetNode
+ */
+function createHighlightedText (highlightingResultNode, targetNode) {
+    let element
+    if (highlightingResultNode.type === 'text') {
+        element = document.createTextNode(highlightingResultNode.text)
+    } else {
+        element = document.createElement('span')
+        element.classList.add('search-result-highlight')
+        element.innerText = highlightingResultNode.text
+    }
+    targetNode.appendChild(element)
+}
+
+
+
+
 
 // disable the search input until the index is loaded
 enableSearchInput(false)
