746722db0e9504b1983d97c8f7256a1a8506fbf1
Andy Damevin <ia3andy@gmail.com>
2025-03-20 11:44:27 +0100

    Add proper exception for template link issues (#449)

---
+++ roq-frontmatter/deployment/src/main/java/io/quarkiverse/roq/frontmatter/deployment/exception/RoqTemplateLinkException.java
@@ -0,0 +1,7 @@
+package io.quarkiverse.roq.frontmatter.deployment.exception;
+
+public class RoqTemplateLinkException extends RuntimeException {
+    public RoqTemplateLinkException(String message) {
+        super(message);
+    }
+}
