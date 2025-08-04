b308749c4124487f6c4b9c3582516f2795ea3e21
tavares-joao <joaotnf@gmail.com>
2025-08-04 09:36:36 -0300

    feat: make social brands configurable via @TemplateGlobal (#584)

--- roq-theme/default/src/main/java/SocialBrands.java
+++ roq-theme/default/src/main/java/SocialBrands.java
@@ -1,25 +1,30 @@
-import io.quarkus.qute.TemplateGlobal;
-import java.util.List;
 import java.util.Map;
+
+import io.quarkus.qute.TemplateGlobal;
 
 @TemplateGlobal
 public class SocialBrands {
+    public static final Map<String, String> iconMap = Map.ofEntries(
+            Map.entry("social-twitter", "fa-brands fa-twitter"),
+            Map.entry("social-github", "fa-brands fa-github"),
+            Map.entry("social-linkedin", "fa-brands fa-linkedin"),
+            Map.entry("social-facebook", "fa-brands fa-facebook"),
+            Map.entry("social-youtube", "fa-brands fa-youtube"),
+            Map.entry("social-discord", "fa-brands fa-discord"),
+            Map.entry("social-email", "fa fa-envelope"),
+            Map.entry("social-bluesky", "fa-brands fa-bluesky"),
+            Map.entry("social-mastodon", "fa-brands fa-mastodon"),
+            Map.entry("social-slack", "fa-brands fa-slack"));
 
-    public static List<Map<String, String>> brands = List.of(
-            Map.of(
-                    "id", "twitter",
-                    "icon", "fa-brands fa-twitter",
-                    "url", "https://twitter.com/quarkusio"
-            ),
-            Map.of(
-                    "id", "github",
-                    "icon", "fa-brands fa-github",
-                    "url", "https://github.com/quarkiverse/quarkus-roq"
-            ),
-            Map.of(
-                    "id", "linkedin",
-                    "icon", "fa-brands fa-linkedin",
-                    "url", "https://linkedin.com/in/quarkusio"
-            )
-    );
+    public static final Map<String, String> urlPrefixMap = Map.ofEntries(
+            Map.entry("social-twitter", "https://twitter.com/"),
+            Map.entry("social-github", "https://github.com/"),
+            Map.entry("social-linkedin", "https://in.linkedin.com/in/"),
+            Map.entry("social-facebook", "https://facebook.com/"),
+            Map.entry("social-youtube", "https://youtube.com/"),
+            Map.entry("social-discord", ""),
+            Map.entry("social-email", "mailto:"),
+            Map.entry("social-bluesky", "https://bsky.app/profile/"),
+            Map.entry("social-mastodon", ""),
+            Map.entry("social-slack", ""));
 }
