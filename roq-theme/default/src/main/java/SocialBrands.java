b3f7f7cac5bdb3c3dfaffd5b24a7560e330b25e5
João Nascimento <69321552+jotaNas@users.noreply.github.com>
2025-08-04 14:07:10 -0300

    Feature/configurable social brands (#585)
    * feat: configurable list of social brands via data
    
    * feat: make social brands configurable via @TemplateGlobal (#584)
    
    * refactor: replace iconMap and urlPrefixMap with SocialBrand record
    
    * formatter:line breack
    
    * Update social brand for Discord

---
+++ roq-theme/default/src/main/java/SocialBrands.java
@@ -0,0 +1,21 @@
+import java.util.Map;
+
+import io.quarkus.qute.TemplateGlobal;
+
+@TemplateGlobal
+public class SocialBrands {
+    private record SocialBrand(String icon, String prefix) {
+    }
+
+    public static final Map<String, SocialBrand> brand = Map.ofEntries(
+            Map.entry("social-twitter", new SocialBrand("fa-brands fa-twitter", "https://twitter.com/")),
+            Map.entry("social-github", new SocialBrand("fa-brands fa-github", "https://github.com/")),
+            Map.entry("social-linkedin", new SocialBrand("fa-brands fa-linkedin", "https://in.linkedin.com/in/")),
+            Map.entry("social-facebook", new SocialBrand("fa-brands fa-facebook", "https://facebook.com/")),
+            Map.entry("social-youtube", new SocialBrand("fa-brands fa-youtube", "https://youtube.com/")),
+            Map.entry("social-discord", new SocialBrand("fa-brands fa-discord", "https://discord.gg/")),
+            Map.entry("social-email", new SocialBrand("fa fa-envelope", "mailto:")),
+            Map.entry("social-bluesky", new SocialBrand("fa-brands fa-bluesky", "https://bsky.app/profile/")),
+            Map.entry("social-mastodon", new SocialBrand("fa-brands fa-mastodon", "")),
+            Map.entry("social-slack", new SocialBrand("fa-brands fa-slack", "")));
+}
