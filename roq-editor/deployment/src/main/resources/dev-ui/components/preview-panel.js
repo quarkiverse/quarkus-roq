f4a8a415ecd9b892615041c1a8e6a8169afef414
Erik Jan de Wit <erikjan.dewit@gmail.com>
2025-12-22 17:20:00 +0100

    added preview tab
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

---
+++ roq-editor/deployment/src/main/resources/dev-ui/components/preview-panel.js
@@ -0,0 +1,54 @@
+import { LitElement, css, html } from 'lit';
+
+export class PreviewPanel extends LitElement {
+
+    static properties = {
+        previewUrl: { type: String }
+    };
+
+    static styles = css`
+        :host {
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+            overflow: hidden;
+        }
+        .preview-container {
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            overflow: hidden;
+        }
+        .preview-iframe {
+            flex: 1;
+            width: 100%;
+            border: none;
+            background: var(--lumo-base-color);
+        }
+    `;
+
+    constructor() {
+        super();
+        this.previewUrl = null;
+    }
+
+
+    render() {
+        if (!this.previewUrl) {
+            return html``;
+        }
+
+        return html`
+            <div class="preview-container">
+                <iframe 
+                    class="preview-iframe"
+                    src="${this.previewUrl}"
+                    title="Preview">
+                </iframe>
+            </div>
+        `;
+    }
+}
+
+customElements.define('qwc-preview-panel', PreviewPanel);
+
