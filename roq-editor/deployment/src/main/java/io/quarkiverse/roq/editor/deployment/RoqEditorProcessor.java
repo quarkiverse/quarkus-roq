f94b4646639b277ef3f4ec6ea0e17baabc03ad4b
Andy Damevin <ia3andy@gmail.com>
2026-01-13 14:36:57 +0100

    Shorcut, Post and Composite

--- roq-editor/deployment/src/main/java/io/quarkiverse/roq/editor/deployment/RoqEditorProcessor.java
+++ roq-editor/deployment/src/main/java/io/quarkiverse/roq/editor/deployment/RoqEditorProcessor.java
@@ -1,21 +1,58 @@
 package io.quarkiverse.roq.editor.deployment;
 
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+
 import io.quarkiverse.roq.editor.runtime.devui.RoqEditorJsonRPCService;
 import io.quarkus.deployment.IsDevelopment;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.Produce;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.LaunchModeBuildItem;
+import io.quarkus.deployment.builditem.ServiceStartBuildItem;
+import io.quarkus.deployment.console.ConsoleCommand;
+import io.quarkus.deployment.console.ConsoleStateManager;
 import io.quarkus.deployment.pkg.builditem.CurateOutcomeBuildItem;
+import io.quarkus.dev.spi.DevModeType;
 import io.quarkus.devui.spi.JsonRPCProvidersBuildItem;
 import io.quarkus.devui.spi.page.CardPageBuildItem;
 import io.quarkus.devui.spi.page.Page;
+import io.quarkus.vertx.http.deployment.HttpRootPathBuildItem;
+import io.quarkus.vertx.http.deployment.NonApplicationRootPathBuildItem;
+import io.quarkus.vertx.http.deployment.devmode.IdeHelper;
 
 class RoqEditorProcessor {
 
     private static final String FEATURE = "roq-editor";
+    static volatile ConsoleStateManager.ConsoleContext context;
 
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FEATURE);
+    }
+
+    @Produce(ServiceStartBuildItem.class)
+    @BuildStep(onlyIf = IsDevelopment.class)
+    void setupConsole(HttpRootPathBuildItem rp, NonApplicationRootPathBuildItem np, LaunchModeBuildItem launchModeBuildItem) {
+        if (launchModeBuildItem.getDevModeType().orElse(null) != DevModeType.LOCAL) {
+            return;
+        }
+        if (context == null) {
+            context = ConsoleStateManager.INSTANCE.createContext("HTTP");
+        }
+        Config c = ConfigProvider.getConfig();
+        String host = c.getOptionalValue("quarkus.http.host", String.class).orElse("localhost");
+        boolean isInsecureDisabled = c.getOptionalValue("quarkus.http.insecure-requests", String.class)
+                .map("disabled"::equals)
+                .orElse(false);
+
+        String port = isInsecureDisabled
+                ? c.getOptionalValue("quarkus.http.ssl-port", String.class).orElse("8443")
+                : c.getOptionalValue("quarkus.http.port", String.class).orElse("8080");
+
+        String protocol = isInsecureDisabled ? "https" : "http";
+        context.reset(new ConsoleCommand('c', "Open the Roq Editor in a browser", null,
+                () -> IdeHelper.openBrowser(rp, np, protocol, "/q/dev-ui/roq-editor", host, port)));
     }
 
     @BuildStep(onlyIf = IsDevelopment.class)
