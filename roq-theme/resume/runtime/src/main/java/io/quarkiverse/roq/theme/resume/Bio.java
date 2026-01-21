1678aa0f31ef2427c3cc5e106e5e9f73cd9212fa
Dimitri Hautot <3765206+DimitriHautot@users.noreply.github.com>
2026-01-21 23:16:04 +0100

    Added new Logo record to Bio record

--- roq-theme/resume/runtime/src/main/java/io/quarkiverse/roq/theme/resume/Bio.java
+++ roq-theme/resume/runtime/src/main/java/io/quarkiverse/roq/theme/resume/Bio.java
@@ -7,8 +7,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.quarkiverse.roq.data.runtime.annotations.DataMapping;
 
 @DataMapping(value = "bio", parentArray = true, required = true)
-public record Bio(
-        List<Section> list) {
+public record Bio(List<Section> list) {
+
     public record Section(@JsonProperty(required = true) String title, List<Item> items) {
     }
 
@@ -16,6 +16,13 @@ public record Bio(
             @JsonProperty(required = true) String header,
             @JsonProperty(required = true) String title,
             String link,
-            String content) {
+            String content,
+            Logo logo) {
+    }
+
+    public record Logo(
+            @JsonProperty(required = true) String label,
+            @JsonProperty(required = true) String imageUrl,
+            String link) {
     }
 }
