e958bfe1098a3d5f70999cc3f8dfef1f78d78e91
jtama <jerome.tama@gmail.com>
2026-04-30 12:41:26 +0200

    Fix devservice leak by reverting to the old devservice runtime model.

--- roq-plugin/diagram/deployment/src/main/java/io/quarkiverse/roq/plugin/diagram/deployment/devservices/DevServicesProcessor.java
+++ roq-plugin/diagram/deployment/src/main/java/io/quarkiverse/roq/plugin/diagram/deployment/devservices/DevServicesProcessor.java
@@ -2,19 +2,23 @@ package io.quarkiverse.roq.plugin.diagram.deployment.devservices;
 
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Supplier;
 
 import org.jboss.logging.Logger;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.utility.DockerImageName;
 
-import io.quarkiverse.roq.plugin.diagram.deployment.RoqPluginDiagramProcessor;
+import io.quarkus.deployment.IsNormal;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.CuratedApplicationShutdownBuildItem;
 import io.quarkus.deployment.builditem.DevServicesResultBuildItem;
 import io.quarkus.deployment.builditem.DockerStatusBuildItem;
 import io.quarkus.deployment.builditem.LaunchModeBuildItem;
-import io.quarkus.deployment.builditem.Startable;
+import io.quarkus.deployment.console.ConsoleInstalledBuildItem;
+import io.quarkus.deployment.console.StartupLogCompressor;
 import io.quarkus.deployment.dev.devservices.DevServicesConfig;
+import io.quarkus.deployment.logging.LoggingSetupBuildItem;
 import io.quarkus.devservices.common.ContainerAddress;
 import io.quarkus.devservices.common.ContainerLocator;
 import io.quarkus.runtime.configuration.ConfigUtils;
@@ -26,20 +30,100 @@ public class DevServicesProcessor {
      * This allows other applications to discover the running service and use it instead of starting a new instance.
      */
     static final String DEV_SERVICE_LABEL = "kroki-dev-service";
-    static final String DEV_SERVICE_NAME = "kroki";
     static final int KROKI_PORT = 8000;
     private static final Logger LOGGER = Logger.getLogger(DevServicesProcessor.class);
     private static final String KROKI_URL_KEY = "quarkus.rest-client.kroki-api.url";
     private static final ContainerLocator krokiContainerLocator = new ContainerLocator(DEV_SERVICE_LABEL, KROKI_PORT);
+    static volatile DevServicesResultBuildItem.RunningDevService devService;
 
-    @BuildStep(onlyIf = DevServicesConfig.Enabled.class)
+    static volatile boolean first = true;
+
+    @BuildStep(onlyIfNot = IsNormal.class, onlyIf = DevServicesConfig.Enabled.class)
     public DevServicesResultBuildItem startEnvProviderDevService(
             DockerStatusBuildItem dockerStatusBuildItem,
             LaunchModeBuildItem launchMode,
+            Optional<ConsoleInstalledBuildItem> consoleInstalledBuildItem,
+            CuratedApplicationShutdownBuildItem closeBuildItem,
+            LoggingSetupBuildItem loggingSetupBuildItem,
             DevServiceConfig krokiDevserviceConfig,
             DevServicesConfig devServicesConfig) {
 
-        if (devServicesDisabled(dockerStatusBuildItem, devServicesConfig, krokiDevserviceConfig)) {
+        StartupLogCompressor compressor = new StartupLogCompressor(
+                (launchMode.isTest() ? "(test) " : "") + "Kroki devservice starting:",
+                consoleInstalledBuildItem, loggingSetupBuildItem);
+        try {
+            devService = startKroki(dockerStatusBuildItem, launchMode, devServicesConfig, krokiDevserviceConfig);
+            if (devService == null) {
+                compressor.closeAndDumpCaptured();
+            } else {
+                compressor.close();
+            }
+        } catch (Throwable t) {
+            compressor.closeAndDumpCaptured();
+            throw t instanceof RuntimeException ? (RuntimeException) t : new RuntimeException(t);
+        }
+
+        if (devService == null) {
+            return null;
+        }
+
+        if (first) {
+            first = false;
+            Runnable closeTask = () -> {
+                if (devService != null) {
+                    shutdownServer();
+                }
+                first = true;
+                devService = null;
+            };
+            closeBuildItem.addCloseTask(closeTask, true);
+        }
+
+        if (devService.isOwner()) {
+            LOGGER.infof("Kroki sev service started on %s", getAcmeEnvUrl());
+            LOGGER.infof("Other Quarkus applications in dev mode will find the "
+                    + "instance automatically. For Quarkus applications in production mode, you can connect to"
+                    + " this by starting your application with -D%s=%s",
+                    KROKI_URL_KEY, getAcmeEnvUrl());
+        }
+
+        return devService.toBuildItem();
+    }
+
+    private Object getAcmeEnvUrl() {
+        return devService.getConfig().get(KROKI_URL_KEY);
+    }
+
+    private void shutdownServer() {
+        if (devService != null) {
+            try {
+                devService.close();
+            } catch (Throwable e) {
+                LOGGER.error("Failed to stop the Minio server", e);
+            } finally {
+                devService = null;
+            }
+        }
+    }
+
+    private DevServicesResultBuildItem.RunningDevService startKroki(
+            DockerStatusBuildItem dockerStatusBuildItem,
+            LaunchModeBuildItem launchMode,
+            DevServicesConfig devServicesConfig, DevServiceConfig krokiDevserviceConfig) {
+        if (!(devServicesConfig.enabled() && krokiDevserviceConfig.enabled())) {
+            // explicitly disabled
+            LOGGER.debug("Not starting dev services for Kroki, as it has been disabled in the config.");
+            return null;
+        }
+
+        // Check if quarkus.kroki.url is set
+        if (ConfigUtils.isPropertyPresent(KROKI_URL_KEY)) {
+            LOGGER.debugf("Not starting dev services for Kroki, the % is configured.", KROKI_URL_KEY);
+            return null;
+        }
+
+        if (!dockerStatusBuildItem.isContainerRuntimeAvailable()) {
+            LOGGER.warnf("Docker isn't working, please configure the Kroki Url property (%s).", KROKI_URL_KEY);
             return null;
         }
 
@@ -47,68 +131,36 @@ public class DevServicesProcessor {
                 true,
                 launchMode.getLaunchMode());
 
-        return maybeContainerAddress.map(containerAddress -> DevServicesResultBuildItem.discovered()
-                .feature(DEV_SERVICE_NAME)
-                .containerId(containerAddress.getId())
-                .config(Map.of(KROKI_URL_KEY,
-                        "http://%s:%d".formatted(containerAddress.getHost(), containerAddress.getPort())))
-                .build())
-                .orElseGet(() -> {
-                    startKroki(devServicesConfig, krokiDevserviceConfig).start();
-                    return DevServicesResultBuildItem.owned()
-                            .feature(RoqPluginDiagramProcessor.FEATURE)
-                            .serviceName(DEV_SERVICE_NAME)
-                            .serviceConfig(krokiDevserviceConfig)
-                            .startable(() -> startKroki(devServicesConfig, krokiDevserviceConfig))
-                            .postStartHook(DevServicesProcessor::logStarted)
-                            .configProvider(Map.of(KROKI_URL_KEY, KrokiContainer::getConnectionInfo))
-                            .build();
-                });
-    }
+        // Starting the server
+        final Supplier<DevServicesResultBuildItem.RunningDevService> defaultAcmeEnvBrokerSupplier = () -> {
+            KrokiContainer container = new KrokiContainer(
+                    DockerImageName.parse(krokiDevserviceConfig.imageName()));
 
-    private static void logStarted(KrokiContainer container) {
-        LOGGER.infof("Kroki dev service started on %s", container.getConnectionInfo());
-        LOGGER.infof("Other Quarkus applications in dev mode will find the "
-                + "instance automatically. For Quarkus applications in production mode, you can connect to"
-                + " this by starting your application with -D%s=%s",
-                KROKI_URL_KEY, container.getConnectionInfo());
-    }
+            container.withLabel(DEV_SERVICE_LABEL, DEV_SERVICE_LABEL);
+            container.addEnv("KROKI_SAFE_MODE", "UNSAFE");
 
-    private KrokiContainer startKroki(
-            DevServicesConfig devServicesConfig, DevServiceConfig krokiDevserviceConfig) {
+            devServicesConfig.timeout().ifPresent(container::withStartupTimeout);
 
-        KrokiContainer container = new KrokiContainer(DockerImageName.parse(krokiDevserviceConfig.imageName()));
-        container.withLabel(DEV_SERVICE_LABEL, DEV_SERVICE_LABEL);
-        container.addEnv("KROKI_SAFE_MODE", "UNSAFE");
-        devServicesConfig.timeout().ifPresent(container::withStartupTimeout);
-        return container;
-    }
+            container.start();
+            return new DevServicesResultBuildItem.RunningDevService(DEV_SERVICE_LABEL,
+                    container.getContainerId(),
+                    container::close,
+                    Map.of(KROKI_URL_KEY, "http://%s:%d".formatted(container.getHost(), container.getPort())));
+        };
 
-    private static boolean devServicesDisabled(DockerStatusBuildItem dockerStatusBuildItem, DevServicesConfig devServicesConfig,
-            DevServiceConfig krokiDevserviceConfig) {
-        if (!(devServicesConfig.enabled() && krokiDevserviceConfig.enabled())) {
-            // explicitly disabled
-            LOGGER.infof("Not starting dev services for Kroki, as it has been disabled in the config.");
-            return true;
-        }
-
-        // Check if quarkus.kroki.url is set
-        if (ConfigUtils.isPropertyPresent(KROKI_URL_KEY)) {
-            LOGGER.infof("Not starting dev services for Kroki, the %s is configured.", KROKI_URL_KEY);
-            return true;
-        }
-
-        if (!dockerStatusBuildItem.isContainerRuntimeAvailable()) {
-            LOGGER.warnf("Docker isn't working, please configure the Kroki Url property (%s).", KROKI_URL_KEY);
-            return true;
-        }
-        return false;
+        return maybeContainerAddress
+                .map(containerAddress -> new DevServicesResultBuildItem.RunningDevService(DEV_SERVICE_LABEL,
+                        containerAddress.getId(),
+                        null,
+                        Map.of(KROKI_URL_KEY,
+                                "http://%s:%d".formatted(containerAddress.getHost(), containerAddress.getPort()))))
+                .orElseGet(defaultAcmeEnvBrokerSupplier);
     }
 
     /**
      * Container configuring and starting the kroki server.
      */
-    private static final class KrokiContainer extends GenericContainer<KrokiContainer> implements Startable {
+    private static final class KrokiContainer extends GenericContainer<KrokiContainer> {
 
         private KrokiContainer(DockerImageName dockerImageName) {
             super(dockerImageName);
@@ -118,16 +170,6 @@ public class DevServicesProcessor {
 
         public int getPort() {
             return getMappedPort(KROKI_PORT);
-        }
-
-        @Override
-        public String getConnectionInfo() {
-            return "http://%s:%s".formatted(getHost(), getPort());
-        }
-
-        @Override
-        public void close() {
-            super.close();
         }
     }
 }
