df1bb6f2745b7ad09d8fc4c2484824f472ccd247
Matheus Cruz <matheuscruz.dev@gmail.com>
2026-06-25 00:41:40 -0300

    Add medium-zoom for image support
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>

--- blog/web/main.js
+++ blog/web/main.js
@@ -1,5 +1,12 @@
 import hljs from 'highlight.js';
 import 'highlight.js/styles/github.css';
 import './_hljs-dark.css';
+import './_medium-zoom.css';
+import mediumZoom from 'medium-zoom';
 
 hljs.highlightAll();
+
+mediumZoom('.content-prose img, .asciidoc-prose img', {
+  background: 'rgba(0, 0, 0, 0.85)',
+  margin: 24,
+});
